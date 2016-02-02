a9ac9614b8787dce404e4c946bab3388ed40654c
Jon H.M. Chan <me@jonhmchan.com>
2016-02-02 02:46:21 -0500

    Better listeners, render() in constructor, helper functions, docs update

--- styles.js
+++ styles.js
@@ -1,6 +1,6 @@
-const m = [{margin: 0, padding: 0, "min-height": "100%"}]
+const m = [{margin: 0, padding: 0, "height": "100%"}]
 const wrapper = {"max-width": 800, margin: "0 auto"}
-const rgba = (a,b,c,d) => { return "rgba("+[a,b,c,d].join()+")" }
+const verticalAlign = {position: "relative", top: "50%", "transform": "translateY(-50%)"}
 const rand_rgba = () => {
 	return "rgba("+[255,255,255].map(x => {
 		return Math.round(Math.random() * x);
@@ -10,16 +10,30 @@ const rand_angle = () => {
 	return Math.round(Math.random() * (180) - 90);
 }
 
-const _ = {
+const prop = (sel, a, b, x, y) => {
+	if (sel <= a) return x
+	if (sel >= b) return y
+	const ratio = (sel-a)/(b-a)
+	return (x + ((y-x) * ratio))
+}
+
+new Descartes({
 	"html": {
 		_mixins: m,
 		"font-family": "Source Sans Pro, Helvetica",
 		color: "#fff",
+		"font-size": 16,
 		body: {
+			_mixins: m,
+			height: "100%",
 			background: () => {
 				return 'linear-gradient(' + rand_angle().toString() + 'deg, ' + rand_rgba() + ', ' + rand_rgba() + ')'
 			},
-			_mixins: m,
+			"pre": {
+				border: "1px solid #fff",
+				"white-space": "pre-wrap",
+				"text-align": "left"
+			},
 			nav: {
 				_mixins: wrapper,
 				"text-align": "center",
@@ -27,21 +41,30 @@ const _ = {
 			},
 			header: {
 				_mixins: m,
+				height: "90%",
 				"div.content": {
-					_listeners: [[window, "resize"]],
-					"margin": (s) => { return (((window.innerHeight - s.offsetHeight) / 2) - 50) + "px 0"; },
+					_listeners: [[window, "scroll"]],
+					_mixins: [verticalAlign, wrapper],
+					"opacity": () => {
+						const top = (window.pageYOffset || document.documentElement.scrollTop)  - (document.documentElement.clientTop || 0)
+						return prop(top, 150, 350, 1, 0);
+					},
 					"text-align": "center",
 					h1: {
 						"font-size": 120,
-						"font-weight": 200,
+						"font-weight": 300,
 						"line-height": 110,
 						margin: 0
 					},
 					h2: {
 						margin: 0,
-						"margin-bottom": 15,
+						"margin-bottom": 0,
 						"font-weight": 200,
 						"font-size": 36
+					},
+					p: {
+						"margin-top": 5,
+						"font-size": 20
 					},
 					button: {
 						background: "none",
@@ -55,7 +78,7 @@ const _ = {
 			},
 			section: {
 				color: "#333",
-				background: "rgba(255,255,255,0.80)",
+				background: "#fff",
 				padding: "25px 0",
 				"> div": {
 					_mixins: wrapper,
@@ -63,7 +86,4 @@ const _ = {
 			}
 		}
 	}
-}
-
-d = new Descartes(_)
-d.render()
+})
