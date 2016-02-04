478e74663578b7170ebeba3baab8573d3bfc2db7
Jon H.M. Chan <me@jonhmchan.com>
2016-02-04 02:06:23 -0500

    Fixed rgba function in Plato

--- src/plato.js
+++ src/plato.js
@@ -12,7 +12,7 @@ class Plato {
 	// ----------
 	// Shorthand for rgba() colors
 	rgba(r=0,g=0,b=0,a=0) {
-		return "rgba(" + [r,g,b,a].join(",") + ")"
+		return "rgba(" + [r,g,b].map(x => Math.round(x)).join(",") + "," + a + ")"
 	}
 
 	// Scale
